b4b9408a7c3a6fcc78125cbfa842dd44ce31488f
Abir Saha <112750923+DevAbir0@users.noreply.github.com>
2022-09-08 05:21:21 +0600

    Add files via upload

---
+++ Petrol-tailwind-project-main/tailwind.config.js
@@ -0,0 +1,37 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        red: "#F40404",
+        yellow: "rgb(250 204 21)",
+        bannerOverlay: "rgba(0, 0, 0, 0.6)",
+        paraColor: "#6C6C6C",
+        comCo: "#F0F0F0",
+        whiteHalf: "#FFFFFF80",
+        foottop: "#1F1F1F",
+        footbot: "#282828",
+        ftext: "rgba(108, 108, 108, 1)",
+      },
+      maxWidth: {
+        conatiner: "1120px",
+      },
+      fontFamily: {
+        pop: "Poppins",
+      },
+      backgroundImage: {
+        banner: "url('BannerBg.png')",
+        serviceOne: "url('redpipe.png')",
+        serviceTwo: "url('welding.png')",
+        serviceThree: "url('refill.png')",
+        company: "url('company.png')",
+        blog1: "url('blog1.png')",
+        blog2: "url('blog2.png')",
+        blog3: "url('blog3.png')",
+        footlog: "url('ftlog.png')",
+        crt1: "url('cert1.png')",
+        crt2: "url('cert2.png')",
+      },
+    },
+  },
+};
